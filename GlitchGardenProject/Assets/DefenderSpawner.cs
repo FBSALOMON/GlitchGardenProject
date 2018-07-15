75884cd499de0d15c5aab28fdec686c4e854841f
FBSALOMON <fbsalomon@gmail.com>
2018-07-15 00:39:07 -0400

    Glitch Garden: A Plants vs. Zombies Clone (43/61)

---
+++ GlitchGardenProject/Assets/DefenderSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+    public Camera myCamera;
+    private GameObject defenderParent;
+
+    void Start()
+    {
+        defenderParent = GameObject.Find("Defenders");
+
+        if (defenderParent == null)
+        {
+            defenderParent = new GameObject("Defenders");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Vector2 rawPos = CalculateWorldPointOfMouseClick();
+        Vector2 roundedPos = SnapToGrid(rawPos);
+        GameObject defender = Button.selectedDefender;
+        Quaternion zeroRot = Quaternion.identity;
+
+        GameObject newDef = Instantiate(defender, roundedPos, zeroRot) as GameObject;
+        newDef.transform.parent = defenderParent.transform;
+    }
+
+    Vector2 CalculateWorldPointOfMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+        return worldPos;
+    }
+
+    Vector2 SnapToGrid (Vector2 rawWorldPos)
+    {
+        int newX = Mathf.RoundToInt(rawWorldPos.x);
+        int newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+}
