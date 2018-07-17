8b2fb577fc80585808b13187a74f957a5ed57f65
FBSALOMON <fbsalomon@gmail.com>
2018-07-17 12:57:04 -0400

    Glitch Garden: A Plants vs. Zombies Clone (61/61)
    minor fixes:
    -sound logo
    -shered and lose colider

---
+++ GlitchGardenProject/Assets/VideoPlayerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerTest : MonoBehaviour
+{
+
+    private void Start()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+        AudioSource audioSource = GetComponent<AudioSource>();
+
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+        videoPlayer.EnableAudioTrack(0, true);
+    }
+    
+}
